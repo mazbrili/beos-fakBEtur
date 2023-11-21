448d4e55a382d61a1e2c028235a5f488b6940ade
mazbrili <mazbrili@yahoo.com>
2023-11-21 08:40:12 +0700

    Update tabfirma.cpp
    translate into english

--- tabfirma.cpp
+++ tabfirma.cpp
@@ -1,8 +1,8 @@
 //
-// część bazodanowa w firmadat, z wyjątkiem wypełniania listy, pomyśleć
-// jak to koszernie rozdzielić
+// database part in company date, except for filling out the list, think
+// how to separate it kosherly
 //
-// część kodu będzie użyta ponownie w zakładce towarowej
+// part of the code will be reused in the goods tab
 //
 
 
@@ -51,11 +51,11 @@ tabFirma::tabFirma(BTabView *tv, sqlite *db, BHandler *hr) : beFakTab(tv, db, hr
 	list->SetInvocationMessage(new BMessage(LIST_INV));
 	list->SetSelectionMessage(new BMessage(LIST_SEL));
 	// buttons
-	but_new = new BButton(BRect(30,0,140,24), "tf_but_new", "Nowy kontrahent [F5]", new BMessage(BUT_NEW), B_FOLLOW_LEFT|B_FOLLOW_TOP);
-	but_del = new BButton(BRect(30,510,140,534), "tf_but_del", "Usuń zaznaczone [F8]", new BMessage(BUT_DEL), B_FOLLOW_LEFT|B_FOLLOW_BOTTOM);
-	but_restore = new BButton(BRect(235,510,325,534), "tf_but_restore", "Przywróć [F6]", new BMessage(BUT_RESTORE), B_FOLLOW_LEFT|B_FOLLOW_BOTTOM);
-	but_save = new BButton(BRect(580,510,670,534), "tf_but_save", "Zapisz", new BMessage(BUT_SAVE), B_FOLLOW_RIGHT|B_FOLLOW_BOTTOM);
-	but_payments = new BButton(BRect(405,510,485,534), "tf_but_payment", "Należności [F9]", new BMessage(BUT_PAYMENT), B_FOLLOW_LEFT_RIGHT|B_FOLLOW_BOTTOM);
+	but_new = new BButton(BRect(30,0,140,24), "tf_but_new", "New contractor [F5]", new BMessage(BUT_NEW), B_FOLLOW_LEFT|B_FOLLOW_TOP);
+	but_del = new BButton(BRect(30,510,140,534), "tf_but_del", "Delete selected [F8]", new BMessage(BUT_DEL), B_FOLLOW_LEFT|B_FOLLOW_BOTTOM);
+	but_restore = new BButton(BRect(235,510,325,534), "tf_but_restore", "Restore [F6]", new BMessage(BUT_RESTORE), B_FOLLOW_LEFT|B_FOLLOW_BOTTOM);
+	but_save = new BButton(BRect(580,510,670,534), "tf_but_save", "Save", new BMessage(BUT_SAVE), B_FOLLOW_RIGHT|B_FOLLOW_BOTTOM);
+	but_payments = new BButton(BRect(405,510,485,534), "tf_but_payment", "Receivables [F9]", new BMessage(BUT_PAYMENT), B_FOLLOW_LEFT_RIGHT|B_FOLLOW_BOTTOM);
 	this->view->AddChild(but_new);
 	this->view->AddChild(but_del);
 	this->view->AddChild(but_restore);
@@ -71,11 +71,11 @@ tabFirma::tabFirma(BTabView *tv, sqlite *db, BHandler *hr) : beFakTab(tv, db, hr
 	box1->SetLabel("Dane adresowe");
 	this->view->AddChild(box1);
 	// box1-stuff
-	data[0] = new BTextControl(BRect(10,15,270,35), "tfd0", "Nazwa", NULL, new BMessage(DC));
+	data[0] = new BTextControl(BRect(10,15,270,35), "tfd0", "Name", NULL, new BMessage(DC));
 	data[1] = new BTextControl(BRect(280,15,420,35), "tfd1", "Symbol", NULL, new BMessage(DC));
-	data[2] = new BTextControl(BRect(10,50,420,65), "tfd2", "Adres", NULL, new BMessage(DC));
-	data[3] = new BTextControl(BRect(10,80,150,95), "tfd3", "Kod", NULL, new BMessage(DC));
-	data[4] = new BTextControl(BRect(160,80,420,95), "tfd4", "Miejscowość", NULL, new BMessage(DC));
+	data[2] = new BTextControl(BRect(10,50,420,65), "tfd2", "Address", NULL, new BMessage(DC));
+	data[3] = new BTextControl(BRect(10,80,150,95), "tfd3", "Code", NULL, new BMessage(DC));
+	data[4] = new BTextControl(BRect(160,80,420,95), "tfd4", "City", NULL, new BMessage(DC));
 	data[5] = new BTextControl(BRect(10,110,200,125), "tfd5", "Tel.", NULL, new BMessage(DC));
 	data[6] = new BTextControl(BRect(210,110,420,125), "tfd6", "Email", NULL, new BMessage(DC));
 	box1->AddChild(data[0]); box1->AddChild(data[1]);
@@ -84,30 +84,30 @@ tabFirma::tabFirma(BTabView *tv, sqlite *db, BHandler *hr) : beFakTab(tv, db, hr
 	box1->AddChild(data[5]); box1->AddChild(data[6]);
 	// box2
 	box2 = new BBox(BRect(230,190,670,340), "tf_box2");
-	box2->SetLabel("Dane firmy");
+	box2->SetLabel("Company Details");
 	this->view->AddChild(box2);
 	// box2-stuff
 	r.left = 10; r.top = 20; r.right = 420; r.bottom = 35;
 	data[7] = new BTextControl(r, "tfd7", "NIP", NULL, new BMessage(DC)); r.OffsetBy(0, 30);
 	data[8] = new BTextControl(r, "tfd8", "REGON", NULL, new BMessage(DC)); r.OffsetBy(0, 30);
 	data[9] = new BTextControl(r, "tfd9", "Bank", NULL, new BMessage(DC)); r.OffsetBy(0, 30);
-	data[10] = new BTextControl(r, "tfd10", "Nr konta", NULL, new BMessage(DC));
+	data[10] = new BTextControl(r, "tfd10", "Account No", NULL, new BMessage(DC));
 	box2->AddChild(data[7]); box2->AddChild(data[8]);
 	box2->AddChild(data[9]); box2->AddChild(data[10]);
 	// box3
 	box3 = new BBox(BRect(230,360,380,440), "tf_box3");
 	this->view->AddChild(box3);
 	// box3-stuff
-	odbiorca = new BCheckBox(BRect(10,15,100,35), "tf_co", "Odbiorca", new BMessage(DC));
-	dostawca = new BCheckBox(BRect(10,50,100,65), "tf_cd", "Dostawca", new BMessage(DC));
-	box3->AddChild(odbiorca); box3->AddChild(dostawca);
+	recipient = new BCheckBox(BRect(10,15,100,35), "tf_co", "Recipient", new BMessage(DC));
+	supplier = new BCheckBox(BRect(10,50,100,65), "tf_cd", "Supplier", new BMessage(DC));
+	box3->AddChild(recipient); box3->AddChild(provider);
 	// box4
 	box4 = new BBox(BRect(520,360,670,440), "tf_box4");
 	this->view->AddChild(box4);
 	// box4-stuff
-	aktywny = new BCheckBox(BRect(10,15,100,35), "tfca", "Aktywny", new BMessage(DC));
-	zablokowany = new BCheckBox(BRect(10,50,100,65), "tf_cz", "Zablokowany", new BMessage(DC));
-	box4->AddChild(aktywny); box4->AddChild(zablokowany);	
+	active = new BCheckBox(BRect(10,15,100,35), "tfca", "Active", new BMessage(DC));
+	blocked = new BCheckBox(BRect(10,50,100,65), "tf_cz", "Blocked", new BMessage(DC));
+	box4->AddChild(active); box4->AddChild(blocked);
 	// fix widths
 	int i;
 	// first set them to be enough
@@ -142,10 +142,10 @@ void tabFirma::curdataFromTab(void) {
 	for (i=0;i<=10;i++) {
 		curdata->data[i] = data[i]->Text();
 	}
-	curdata->odbiorca = (odbiorca->Value() == B_CONTROL_ON);
-	curdata->dostawca = (dostawca->Value() == B_CONTROL_ON);
-	curdata->aktywny = (aktywny->Value() == B_CONTROL_ON);
-	curdata->zablokowany = (zablokowany->Value() == B_CONTROL_ON);
+	curdata->recipient = (recipient->Value() == B_CONTROL_ON);
+	curdata->supplier = (supplier->Value() == B_CONTROL_ON);
+	curdata->active = (active->Value() == B_CONTROL_ON);
+	curdata->blocked = (blocked->Value() == B_CONTROL_ON);
 }
 
 void tabFirma::curdataToTab(void) {
@@ -153,10 +153,10 @@ void tabFirma::curdataToTab(void) {
 	for (i=0;i<=10;i++) {
 		data[i]->SetText(curdata->data[i].String());
 	}
-	odbiorca->SetValue(curdata->odbiorca ? B_CONTROL_ON : B_CONTROL_OFF);
-	dostawca->SetValue(curdata->dostawca ? B_CONTROL_ON : B_CONTROL_OFF);
-	aktywny->SetValue(curdata->aktywny ? B_CONTROL_ON : B_CONTROL_OFF);
-	zablokowany->SetValue(curdata->zablokowany ? B_CONTROL_ON : B_CONTROL_OFF);
+	recipient->SetValue(curdata->recipient ? B_CONTROL_ON : B_CONTROL_OFF);
+	supplier->SetValue(curdata->supplier ? B_CONTROL_ON : B_CONTROL_OFF);
+	active->SetValue(curdata->active ? B_CONTROL_ON : B_CONTROL_OFF);
+	blocked->SetValue(curdata->blocked ? B_CONTROL_ON : B_CONTROL_OFF);
 	updateTab();
 }
 
@@ -170,9 +170,9 @@ void tabFirma::updateTab(void) {
 	for (i=0;i<=10;i++) {
 		data[i]->SetEnabled(state);
 	}
-	odbiorca->SetEnabled(state);
-	dostawca->SetEnabled(state);
-	aktywny->SetEnabled(state);
+	recipient->SetEnabled(state);
+	provider->SetEnabled(state);
+	active->SetEnabled(state);
 }
 
 // perform checks against supplied data
@@ -180,67 +180,67 @@ bool tabFirma::validateTab(void) {
 	BAlert *error;
 	BString sql, tmp;
 	int i;
-	// nazwa - niepusta
+	// name - not empty
 	if (strlen(data[0]->Text()) == 0) {
-		error = new BAlert(APP_NAME, "Nie wpisano nazwy kontrahenta!", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The contractor's name was not entered!", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		error->Go();
 		data[0]->MakeFocus();
 		return false;
 	}
-	// nazwa - unikalna
+	// name - unique
 	tmp = data[0]->Text(); tmp.ReplaceAll("'","''");	// sql quote
 	sql = "SELECT id FROM firma WHERE nazwa = '"; sql += tmp; sql += "'";
 	i = toint(execSQL(sql.String()));
 	if (((curdata->id < 0) && ( i!= 0 )) || ((curdata->id > 0) && (i != 0) && (i != curdata->id))) {
-		error = new BAlert(APP_NAME, "Nazwa firmy nie jest unikalna!", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The company name is not unique!", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		error->Go();
 		data[0]->MakeFocus();
 		return false;
 	}
-	// symbol - niepusty
+	// symbol - not empty
 	if (strlen(data[1]->Text()) == 0) {
-		error = new BAlert(APP_NAME, "Nie wpisano symbolu kontrahenta!", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The contractor's symbol has not been entered", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		error->Go();
 		data[1]->MakeFocus();
 		return false;
 	}
-	// symbol - unikalny
+	// symbol - unique
 	tmp = data[1]->Text(); tmp.ReplaceAll("'","''");	// sql quote
 	sql = "SELECT id FROM firma WHERE symbol = '"; sql += tmp; sql += "'";
 	i = toint(execSQL(sql.String()));
 	if (((curdata->id < 0) && ( i!= 0 )) || ((curdata->id > 0) && (i != 0) && (i != curdata->id))) {
-		error = new BAlert(APP_NAME, "Symbol kontrahenta nie jest unikalny!", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The contractor's symbol is not unique!", "OK", NULL, NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		error->Go();
 		data[1]->MakeFocus();
 		return false;
 	}
-	// adres - wszystkie dane
+	// address - all data
 	if ((strlen(data[2]->Text())==0) || (strlen(data[3]->Text())==0) || (strlen(data[4]->Text())==0)) {
-		error = new BAlert(APP_NAME, "Adres kontrahenta jest niepełny.\nKontynuować?", "Tak", "Nie", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The contractor's address is not complete.\Continue?", "Yes", "No", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		if (error->Go() == 1) {
 			data[2]->MakeFocus();
 			return false;
 		}
 	}
-	// NIP - niepusty,poprawny
+	// NIP - not empty, correct
 	if (strlen(data[7]->Text())==0) {
-		error = new BAlert(APP_NAME, "Nie wpisano numeru NIP kontrahenta.\nKontynuować?", "Tak", "Nie", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The contractor's Tax Identification Number has not been entered.\nContinue?",  "Yes", "No", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		if (error->Go() == 1) {
 			data[7]->MakeFocus();
 			return false;
 		}
 	}
-	// REGON - niepusty,poprawny
+	// REGON - not empty, correct
 	if (strlen(data[8]->Text())==0) {
-		error = new BAlert(APP_NAME, "Nie wpisano numeru REGON kontrahenta.\nKontynuować?", "Tak", "Nie", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The contractor's REGON number has not been entered.\nContinue?",  "Yes", "No", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		if (error->Go() == 1) {
 			data[8]->MakeFocus();
 			return false;
 		}
 	}
-	// nr konta - niepusty,poprawny
+	// account number - not empty, correct
 	if (strlen(data[10]->Text())==0) {
-		error = new BAlert(APP_NAME, "Nie wpisanu numeru konta kontrahenta.\nKontynuować?", "Tak", "Nie", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
+		error = new BAlert(APP_NAME, "The contractor's account number has not been entered.\nContinue?",  "Yes", "No", NULL, B_WIDTH_AS_USUAL, B_WARNING_ALERT);
 		if (error->Go() == 1) {
 			data[10]->MakeFocus();
 			return false;
